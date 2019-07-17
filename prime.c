e039e3198125a1ae3a659752e310eaed6a30604b
Jagadeeshchinna <51699762+Jagadeeshchinna@users.noreply.github.com>
2019-07-17 10:38:45 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+int n,i,flag=1;
+scanf("%d",&n);
+for(i = 2;i <= n; i++)
+{
+	if(n%i==0)
+	
+    {
+    	flag++;
+	
+	
+	}	
+}
+if (flag == 2)
+{
+	printf("yes");
+}
+else
+	printf("no");
+	return 0;
+	
+}
